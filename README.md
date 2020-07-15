988bde04be363218a3b11a18038f80b70acaa35e
Eddie Cheng <local@example.test>
2020-07-15 14:07:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Beginner React Hook Tutorial
 
 Code for YouTube series: https://www.youtube.com/playlist?list=PLN3n1USn4xlmyw3ebYuZmGp60mcENitdM
+
+## Usage
+1. npm install
+2. npm start
+3. than do the stuff!
--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
   const [values, handleChange] = useForm({ email: "", password: "" });
-  const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+  // const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+
+  // Everytime, this component re-renders or is rendered, 
+  // this function will get called
+  // the second parameter (dependency array) -> value your effect depends on
+  useEffect(()=> {
+    console.log("render!");
+  }, [values.password]); // effect depends on password
 
   return (
     <div>
